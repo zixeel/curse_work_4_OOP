cf43353f0889070ff98156d04adb2e344d5ec407
zixeel <strpan549@gmail.com>
2023-10-09 18:07:52 +0300

    add Vacancy class & fix base

--- src/api_cls.py
+++ src/api_cls.py
@@ -1,3 +1,4 @@
+import json
 import os
 from abc import ABC, abstractmethod
 
@@ -5,6 +6,11 @@ import requests as requests
 from dotenv import load_dotenv
 
 load_dotenv()
+
+class UrlError(Exception):
+    """Класс ошибки в URL адресе"""
+    def __init__(self, msg):
+        super.__init__(msg)
 
 
 class ApiLoad(ABC):
@@ -14,7 +20,7 @@ class ApiLoad(ABC):
 
 
 class HhLoad(ApiLoad):
-    def __init__(self,job_title):
+    def __init__(self, job_title):
         self.api_url = "https://api.hh.ru/vacancies"
         self.job_title = job_title
         self.options = {'per_page': 100,
@@ -25,7 +31,7 @@ class HhLoad(ApiLoad):
 
     def get_job(self):
         response = requests.get(self.api_url, params=self.options)
-        print(response.json())
+        return response.json()['items']
 
 
 class SuperJobLoad(ApiLoad):
@@ -39,5 +45,47 @@ class SuperJobLoad(ApiLoad):
 
     def get_job(self):
         response = requests.get(self.api_url, params=self.options, headers=self.headers)
-        print(response.json())
+        return response.json()['objects']
+
+    class Vacancy:
+        """Класс, который позволяет создавать экземпляры вакансий и осуществлять с ними работу"""
+        __slots__ = {'name', 'url', 'salary', 'requirement'}
+
+        def __init__(self,name: str, url: str, salary: int, requirement: str):
+            self.name = name
+            if not isinstance(self.name, str):
+                raise TypeError("Название вакансии должно быть типа 'str'")
+            self.url = url
+            if self.url[:8] != 'https://':
+                raise UrlError("Ссылка должна начинаться с https://")
+            self.salary = salary
+            if self.salary is None:
+                raise AttributeError('Поле не может быть пустым')
+            self.requirement = requirement
+            if self.requirement is None:
+                raise AttributeError('Поле не может быть пустым')
+
+        def __str__(self):
+            return f'Название вакансии - {self.name}\n' \
+                   f'Ссылка - {self.url}\n' \
+                   f'З/п до {self.salary} RUR\n' \
+                   f'Требования - {self.requirement}\n'
+
+        def __eq__(self, other):
+            return self.salary == other.salary
+
+        def __ne__(self, other):
+            return self.salary != other.salary
+
+        def __lt__(self, other):
+            return self.salary < other.salary
+
+        def __le__(self, other):
+            return self.salary <= other.salary
+
+        def __gt__(self, other):
+            return self.salary > other.salary
+
+        def __ge__(self, other):
+            return self.salary >= other.salary
 
